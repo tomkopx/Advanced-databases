267d7a0cb649c9094bc9d553503b4a3a2c8cfea7
Tom Cebula <40173513@live.napier.ac.uk>
2017-03-17 12:59:03 +0000

    Coursework finished

--- Coursework/Queries.sql
+++ Coursework/Queries.sql
@@ -45,35 +45,83 @@ Number of savings accounts 				Street                         City              
 									  
 									  
 									  
-									  
---Question 4c
-
-SELECT max(c.accNum.balance), c.accNum.bID.bID
-FROM CustomerAccountTable c
-WHERE c.accNum.accType LIKE 'savings'
-GROUP BY c.accNum.bID;
-
+									 
 
 
 --Question 4d
 
-SELECT e.position.bID.bAddress.street, e.position.bID.bAddress.city, e.position.bID.bAddress.postCode
-FROM EmployeeTable e, BranchTable b
-WHERE e.supervisorID.position.position LIKE 'Manager' AND e.niNum IN (SELECT c.niNum
-                                                                      FROM CustomerTable c);
-																	  
-																	  
---Question 4e
+COLUMN position.bID.bAddress.street HEADING 'Working Branch street'
+COLUMN position.bID.bAddress.city HEADING 'Working Branch city'
+COLUMN position.bID.bAddress.postCode FORMAT A25 HEADING 'Working Branch post code'
+COLUMN accNum.bID.bAddress.street HEADING 'Account Branch street'
+COLUMN accNum.bID.bAddress.city HEADING 'Account Branch city'
+COLUMN accNum.bID.bAddress.postCode FORMAT A25 HEADING 'Account Branch post code'
+SELECT e.position.bID.bAddress.street , e.position.bID.bAddress.city ,e.position.bID.bAddress.postCode , c.accNum.Bid.bAddress.street, c.accNum.Bid.bAddress.city, c.accNum.Bid.bAddress.postcode
+FROM CustomerAccountTable c , EmployeeTable e 
+WHERE c.custID.niNum = e.niNum  AND e.supervisorID IS NOT NULL ;
+--Checking whether or not the employees national insurance number appears in the customer table and then checking to make sure 
+--that the employee also has a supervisor 
 
+--Output                                                                     
+
+Working Branch street          Working Branch city  Working Branch post code  Account Branch street          Account Branch city  Account Branch post code
+------------------------------ -------------------- ------------------------- ------------------------------ -------------------- -------------------------
+Bridge                         Glasgow              G18 1QQ                   Market                         Edinburgh            EH1 5AB                  
+Bridge                         Glasgow              G18 1QQ                   Market                         Edinburgh            EH1 5AB                  
+Heavy                          Glasgow              G12 6RT                   Big                            Glasgow              G14 2BB                  
+Heavy                          Glasgow              G12 6RT                   Big                            Glasgow              G14 2BB                  
+Big                            Glasgow              G14 2BB                   Heavy                          Glasgow              G12 6RT                  
+Big                            Glasgow              G14 2BB                   Heavy                          Glasgow              G12 6RT                  
+Market                         Edinburgh            EH1 5AB                   Burly                          Edinburgh            EH7 1FD                  
+Market                         Edinburgh            EH1 5AB                   Burly                          Edinburgh            EH7 1FD                  
+Heavy                          Glasgow              G12 6RT                   Big                            Glasgow              G14 2BB                  
+Heavy                          Glasgow              G12 6RT                   Big                            Glasgow              G14 2BB                  
+Big                            Glasgow              G14 2BB                   Rowling                        Glasgow              G16 8XS                  
+
+Working Branch street          Working Branch city  Working Branch post code  Account Branch street          Account Branch city  Account Branch post code
+------------------------------ -------------------- ------------------------- ------------------------------ -------------------- -------------------------
+Various                        Glasgow              G15 8NB                   Rowling                        Glasgow              G16 8XS                  
+Heavy                          Glasgow              G12 6RT                   Various                        Glasgow              G15 8NB                  
+Heavy                          Glasgow              G12 6RT                   Various                        Glasgow              G15 8NB                  
+Burly                          Edinburgh            EH7 1FD                   Massive                        Edinburgh            EH3 3KL                  
+Various                        Glasgow              G15 8NB                   Bridge                         Glasgow              G18 1QQ                  
+Heavy                          Glasgow              G12 6RT                   Air                            Glasgow              G14 7TR                  
+Air                            Glasgow              G14 7TR                   Heavy                          Glasgow              G12 6RT                  
+Air                            Glasgow              G14 7TR                   Heavy                          Glasgow              G12 6RT                  
+Various                        Glasgow              G15 8NB                   Collecting                     Glasgow              G14 7YT                  
+Big                            Glasgow              G14 2BB                   Bridge                         Glasgow              G18 1QQ                  
+Market                         Edinburgh            EH1 5AB                   Grant                          Edinburgh            EH2 7CB                  
+
+Working Branch street          Working Branch city  Working Branch post code  Account Branch street          Account Branch city  Account Branch post code
+------------------------------ -------------------- ------------------------- ------------------------------ -------------------- -------------------------
+Market                         Edinburgh            EH1 5AB                   Grant                          Edinburgh            EH2 7CB                  																  
 
 
 
 --Question 4f
 
-SELECT c.pName.firstName, t.*
-FROM CustomerTable c, table(c.pPhone.mobilePhone) t
-WHERE (SELECT count(*) FROM table(c.pPhone.mobilePhone) t) > 1; 
 
+SELECT c.pName.firstName, c.pName.surName, t.*
+FROM CustomerTable c, table(c.pPhone.mobilePhone) t
+WHERE (SELECT count(*) FROM table(c.pPhone.mobilePhone) t) > 1 AND '0770' IN (SELECT SUBSTR(t.column_value, 0,4) FROM table(c.pPhone.mobilePhone) t); 
+--The first nested select statement counts the number of numbers each customer has, then checks whether or not its more than 1
+--The next nested select statement selects a substring of each mobile number (first 4 characters) and 
+--sees whether or not 0770 is in the nested table.
+
+--Output
+
+First Name           Last Name            Mobile Number      
+-------------------- -------------------- --------------------
+Jack                 Smith                0781209890          
+Jack                 Smith                0770234567          
+Jenny                Peacock              0770209142          
+Jenny                Peacock              0770208242          
+John                 Gabriels             0770123456          
+John                 Gabriels             0770123489          
+John                 Gabriels             0770125436          
+Sarah                Marah                0770209888          
+Sarah                Marah                0781209858          
+Sarah                Marah                0781209840     
 
 
 
@@ -85,7 +133,7 @@ COLUMN Super FORMAT A30 HEADING 'Number of supervised employees'
 COLUMN supervisorID.empID HEADING 'Supervisor ID'
 SELECT e.pName.firstName, e.pName.surName, (SELECT COUNT(e.supervisorID) FROM EmployeeTable e WHERE e.supervisorID.empID = 105) AS Super, e.supervisorID.empID
 FROM EmployeeTable e
-WHERE e.empID = 105;
+WHERE e.pName.surName LIKE 'William' AND e.pName.title LIKE 'Mr';
 --Nested select statement to basically count all the supervised employees.
 --FORMAT A30 to make sure the column width is good enough to fit heading.
 --Basically whats happening here is im finding the number of supervised employees by John William who is
